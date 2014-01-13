21f6f3a3f15c0775f4fd4ebdc4e34d7add2a284e
Mikolaj Kowalik <mxk62@psu.edu>
2014-01-13 18:05:56 -0500

    Extend test unit for find_core() method
    I added more example reactions with their expected cores to unit test of
    find_core() method of Reaction class.

--- test_reaction.py
+++ test_reaction.py
@@ -4,10 +4,79 @@ from reaction import Reaction
 def test_find_core():
     """Returns True, if core was extracted correctly."""
 
-    # Test if it work properly at all.
-    smiles = 'CC(=O)CC(C)C(CC#N)C(=O)N>>CC(=O)CC(C)C(CC#N)C#N'
+    # Working examples from the original paper by Lynch and Willett.
+    smiles = '>>'.join([
+        'CC(=O)CC(C)C(CC#N)C(=O)N',
+        'CC(=O)CC(C)C(CC#N)C#N'
+    ])
     rxn = Reaction(smiles)
-    assert rxn.find_core == 'N#CC.C(=O)N>>N#CC.N#C'
+    assert rxn.find_core == 'CC(N)>>CC#N'
+
+    smiles = '>>'.join([
+        'NC(=O)C(c1ccccc1)(c1ccccc1)NS(=O)(=O)c1ccccc1',
+        'O=C(O)C(NS(=O)(=O)c1ccccc1)(c1ccccc1)c1ccccc1'
+    ])
+    rxn = Reaction(smiles)
+    assert rxn.find_core == 'NC=O>>OC=O'
+
+    smiles = '>>'.join([
+        'CCC(c1cc(OC)c(OC(C)=O)cc1Cc1ccc(OC(C)=O)c(OC)c1)C(C)OC(C)=O',
+        'CCC(c1cc(OC)c(OC(C)=O)cc1C(=O)c1ccc(OC(C)=O)c(OC)c1)C(C)OC(C)=O'
+    ])
+    rxn = Reaction(smiles)
+    assert rxn.find_core == 'cCc>>cC(c)=O'
+
+    smiles = '>>'.join([
+        'ON(O)c1ccccc1S(=O)(=O)N(C)c1ccccc1',
+        'Nc1ccccc1S(=O)(=O)N(C)c1ccccc1'
+    ])
+    rxn = Reaction(smiles)
+    assert rxn.find_core == 'cN(O)(O)>>cN'
+
+    smiles = '>>'.join([
+        'COc1ccc2c(c1)sc1c2CCC2C1=CCC2O',
+        'COc1ccc2c(c1)sc1c2CCC2C1CCC2O'
+    ])
+    rxn = Reaction(smiles)
+    assert rxn.find_core == 'C=CC>>CCC
+
+
+    # Problematic reactions, violating algorithm assumption---no atom should
+    # be deleted; also from the paper by Lynch and Willett.
+    #
+    #
+    # Ambiguous mapping.
+    #
+    # RDKit (2013_09_01) is having problem with this SMILES so we are skipping
+    # them for now.
+    #smiles = '>>'.join([
+    #    'c1ccc2c(c1)C=Cc1ccccc1NN2',
+    #    'CN1Nc2ccccc2C=Cc2c1cccc2'
+    #])
+    #rxn = Reaction(smiles)
+    #assert rxn.find_core == smiles
+
+    smiles = '>>'.join([
+        'N=C1ON=C2CCCCC12',
+        'Nc1on=c2c=1CCCC2'
+    ])
+    rxn = Reaction(smiles)
+    assert rxn.find_core == smiles
+
+    # Match radius is to small.
+    smiles = '>>'.join([
+        'CC(C)(O)C1CCCO1',
+        'CC1=COCCC1C'
+    ])
+    rxn = Reaction(smiles)
+    assert rxn.find_core == smiles
+
+    smiles = '>>'.join([
+        'O=CC1C=CCC=C1',
+        'O=CC1=CCCC=C1'
+    ])
+    rxn = Reaction(smiles)
+    assert rxn.find_core == smiles
 
 
 def test_make_ec_map():
