f1d955c84b0f8f01ffad4ace66389b3235a5f8e7
Mikolaj Kowalik <mxk62@psu.edu>
2014-04-28 14:31:25 -0400

    Fix formatting issues in Chemical's definition
    Few lines in Chemical class definition were not formatted properly.  I
    changed the formatting according to PEP8 guidelines.

--- mapper/test/test_chemical.py
+++ mapper/test/test_chemical.py
@@ -51,25 +51,25 @@ def test_calc_init_ecs():
     chem = Chemical('CCO')
     correct_indices = (61, 62, 81)
     assert chem.ec_order is None and not chem.ec_indices and \
-           chem.calc_init_ecs() == correct_indices
+        chem.calc_init_ecs() == correct_indices
 
     # A simple linear molecule, Shelley-Munk method.
     chem = Chemical('CCO')
     correct_indices = (21, 22, 41)
     assert chem.ec_order is None and not chem.ec_indices and \
-           chem.calc_init_ecs(index_type='shelley') == correct_indices
+        chem.calc_init_ecs(index_type='shelley') == correct_indices
 
     # A simple linear molecule, unknown method (defaults to Funatsu).
     chem = Chemical('CCO')
     correct_indices = (61, 62, 81)
     assert chem.ec_order is None and not chem.ec_indices and \
-           chem.calc_init_ecs(index_type='undef') == correct_indices
+        chem.calc_init_ecs(index_type='undef') == correct_indices
 
     # Aliphatic fused rings, original Morgan method.
     chem = Chemical('CC1CCCC2CCCC(C)C12')
     correct_indices = (1, 3, 2, 2, 2, 3, 2, 2, 2, 3, 1, 3)
     assert chem.ec_order is None and not chem.ec_indices and \
-           chem.calc_init_ecs(index_type='morgan') == correct_indices
+        chem.calc_init_ecs(index_type='morgan') == correct_indices
 
 
 def test_calc_next_ecs():
@@ -84,7 +84,7 @@ def test_calc_next_ecs():
     chem = Chemical('CCO')
     correct_indices = (184, 266, 224)
     assert chem.ec_order is None and not chem.ec_indices and \
-           chem.calc_next_ecs() == correct_indices
+        chem.calc_next_ecs() == correct_indices
 
     # A simple linear molecule with initialized with Morgan method.
     chem = Chemical('CCO')
@@ -101,15 +101,15 @@ def test_remove_atoms():
     # Removing terminating atom.
     chem = Chemical('CCO')
     chem.remove_atoms([0])
-    expected_smiles = set(['CO'])
+    expected_smiles = {'CO'}
     generated_smiles = set([s for s in chem.smiles.split('.')])
     print generated_smiles
-    assert generated_smiles.issubset(expected_smiles) == True
+    assert generated_smiles.issubset(expected_smiles) is True
 
     # Removing atoms leaves disjoint fragments.
     chem = Chemical('CC(=O)CC(C)C(CC#N)C(=O)N')
     chem.remove_atoms([0, 1, 2, 3, 4, 5, 6])
-    expected_smiles = set(['CC#N', 'NC=O'])
+    expected_smiles = {'CC#N', 'NC=O'}
     generated_smiles = set([s for s in chem.smiles.split('.')])
     print generated_smiles
-    assert generated_smiles.issubset(expected_smiles) == True
+    assert generated_smiles.issubset(expected_smiles) is True
