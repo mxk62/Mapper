752a536ddb7c0eb33331f0d0386ad6429c45eee1
Mikolaj Kowalik <mxk62@psu.edu>
2014-05-01 11:59:26 -0400

    Rewrite docstring in Chemical class
    I rewrote and reformat docstrings in Chemical class in the same manner
    as in Core class in order to use Sphinx document generator.

--- mapper/chemical.py
+++ mapper/chemical.py
@@ -1,3 +1,6 @@
+"""A class representing a chemical substance."""
+
+
 import numpy
 from rdkit import Chem
 
@@ -7,6 +10,19 @@ class Chemical:
     multipliers = {'lynch-willet': 2, 'funatsu': 4}
 
     def __init__(self, smiles, ec_type='funatsu'):
+        """Initializes a chemical.
+
+        Parameters
+        ----------
+        smiles : string
+            A representation of a chemical compound in Daylight SMILES
+            notation.
+
+        Raises
+        ------
+        ValueError
+            Exception is thrown if the input SMILES in invalid.
+        """
         self.mol = Chem.MolFromSmiles(smiles)
         if self.mol is None:
             raise ValueError('Invalid chemical SMILES.')
@@ -37,11 +53,23 @@ class Chemical:
         self.multi = Chemical.multipliers.get(ec_type, 2)
 
     def calc_init_ecs(self, index_type='funatsu'):
-        """Calculates and returns a tuple representing initial EC indices.
+        """Calculates initial EC indices.
 
-        Function returns a tuple in which the $i$-th element represent the EC
-        index of the corresponding atom. It does NOT update their values on
-        atoms.
+        Parameters
+        ----------
+        index_type : string
+            Initial values of EC indices can be calculated using three
+            different methods called 'funatsu', 'shelley', and 'morgan'.
+            Defaults to 'funatsu'.
+
+        Returns
+        -------
+        indices : tuple
+            A sequence in which the $i$-th element represent the EC
+            index of the corresponding atom.
+
+            .. warning::
+            It does *not* update EC values on atoms.
         """
         methods = {
             'funatsu': self.get_funatsu_identifier,
@@ -52,19 +80,26 @@ class Chemical:
         return tuple([get_identifier(a) for a in self.mol.GetAtoms()])
 
     def calc_next_ecs(self):
-        """Calculates and returns a tuple representing next EC indices.
+        """Calculates next EC indices.
 
         Function calculates next order EC indices for molecule's atom.
         The indices are incremented according to the formula
-        \[
-            EC_{i}^{n} = m * EC_{i}^{n - 1} + \sum_{j} EC_{j}^{n - 1},
-        \]
-        where $m$ is method depenendent multiplier (4 for 'funatsu',
+
+        .. math::
+        EC_{i}^{n} = m * EC_{i}^{n - 1} + \sum_{j} EC_{j}^{n - 1},
+
+        where :math:`m` is method dependent multiplier (4 for 'funatsu',
         2 otherwise) and the summation goes over atoms adjacent to  atom
         $i$.
 
-        It returns a tuple in which the $i$-th element represent the EC index
-        of the corresponding atom. It does NOT updates their values on atoms.
+        Returns
+        -------
+        indices : tuple
+            A sequence in which the $i$-th element represent the EC index
+            of the corresponding atom.
+
+            .. warning::
+            It does NOT updates their values on atoms.
         """
 
         # If EC indices were not initialized, do it now.
@@ -78,12 +113,18 @@ class Chemical:
         return tuple(self.multi * ecs + numpy.dot(self.adjacency_matrix, ecs))
 
     def clear_ecs(self):
-        """Clear EC indices from molecule's atoms."""
+        """Clears EC indices."""
         self.ec_order = None
         self.ec_indices = []
 
     def update_ecs(self, ecs):
-        """Updates EC indices on atoms and adjust their order."""
+        """Updates EC indices.
+
+        Parameters
+        ----------
+        ecs : sequence
+            A sequence of integer representing atoms' EC indices.
+        """
         if self.ec_order is None:
             self.ec_order = 1
         else:
@@ -91,24 +132,35 @@ class Chemical:
         self.ec_indices = ecs
 
     def find_ec_mcs(self, index, radius):
-        """Returns indices of all atoms belonging to a given EC-MCS.
+        """Finds all atoms belonging to a given EC-MCS.
 
-        The atom's $n$-order EC index may be treated as a hash of a circular
-        substructure with a radius $(n - 2)$ bonds. Function returns indices of
-        all atoms lying WITHIN, i.e. atoms separated by at most $r - 1$ bonds
-        from the center atom.
+        The atom's :math:`n`-order EC index may be treated as a hash of a
+        circular substructure with a radius :math:`r \equiv (n - 2)` bonds.
+
+        Parameters
+        ----------
+        index : integer
+            An index of an atom for which EC-MCS will be determined
+
+        radius : integer
+            Match radius.
+
+        Returns
+        -------
+        indices : list
+            Indices of all atoms lying *within* match radius, i.e. atoms
+            separated by at most :math:`r - 1` bonds from the center atom.
         """
         return [idx for idx, dist in enumerate(self.distance_matrix[index])
                 if dist < radius]
 
     def remove_atoms(self, indices):
-        """Removes atoms with given indices.
+        """Removes atoms with given indices."""
 
-        Every time an atom with is removed from a molecule, atom indices
-        higher than the deleted atom's index are adjusted. Thus, we are
-        removing them in reversed order, i.e. starting from the atom with
-        highest.
-        """
+        # Every time an atom with is removed from a molecule, atom indices
+        # higher than the deleted atom's index are adjusted. Thus, we are
+        # removing them in reversed order, i.e. starting from the atom with
+        # highest.
         e = Chem.EditableMol(self.mol)
         for i in sorted(indices, reverse=True):
             e.RemoveAtom(i)
@@ -118,35 +170,66 @@ class Chemical:
         self.smiles = Chem.MolToSmiles(self.mol)
 
     def get_atom_classes(self, atom_indices):
-        """Returns a set representing atoms' classes.
+        """Finds out atoms' classes.
 
         Method finds out the classes of atoms (type and bond pattern)
         from the list of indices supplied as its input argument. Currently,
         it determines an atom's class using method described by Shelley and
         Munk.
+
+        Parameters
+        ----------
+        atom_indices : sequence
+            Indices of atoms that classes need to be determined.
+
+        Returns
+        -------
+        classes : set
+            A set of integers representing atoms' classes.
         """
         return set([self.get_shelley_identifier(self.mol.GetAtomWithIdx(i))
                     for i in atom_indices])
 
     @staticmethod
     def get_funatsu_identifier(atom):
-        """Calculates and returns an initial EC index of a given atom.
+        """Calculates an initial EC index of a given atom.
 
         After Funatsu et al. in Tetrahedron Computer Methodology 1,
         53-69 (1988), the values are calculated according to formula
-        \[
-            EC^{1}_{a_{i}} = 10 Z_{a_{i}} + \deg(a_{i})
-        \]
-        where $Z_{a_{i}}$ is the atomic number of $i$-th atom.
+
+        .. math::
+        EC^{1}_{a_{i}} = 10 Z_{a_{i}} + \deg(a_{i})
+
+        where :math:`Z_{a_{i}}` is the atomic number of :math:`i`-th atom.
+
+        Parameters
+        ----------
+        atom : Atom
+            An atom for which calculate index.
+
+        Returns
+        -------
+        index : integer
+            An integer representing initial EC index for a given atom.
         """
         return 10 * atom.GetAtomicNum() + len(atom.GetNeighbors())
 
     @staticmethod
     def get_morgan_identifier(atom):
-        """Calculates and returns an initial EC index of a given atom.
+        """Calculates an initial EC index of a given atom.
 
-        After Morgan in J. Chem. Doc. 5, 107 (1965), the values is just number
+        After Morgan in J. Chem. Doc. 5, 107 (1965), the value is just number
         of nonhydrogen neighbours.
+
+        Parameters
+        ----------
+        atom : Atom
+            An atom for which calculate index.
+
+        Returns
+        -------
+        index : integer
+            An integer representing initial EC index for a given atom.
         """
         return len(atom.GetNeighbors())
 
@@ -158,7 +241,18 @@ class Chemical:
         the values are two-digit integer, the most significant of which
         specifies number of covalent (two-electrons) bonds by which it joins
         nonhydrogen atoms, and the least significant of which designates
-        atom elemental type (i.e. C = 2, N = 3, O = 4, etc.)
+        atom elemental type (i.e. :math:`C = 2`, :math:`N = 3`, :math:`O = 4`,
+        etc.)
+
+        Parameters
+        ----------
+        atom : Atom
+            An atom for which calculate index.
+
+        Returns
+        -------
+        index : integer
+            An integer representing initial EC index for a given atom.
         """
 
         # Get atom's type. If not defined, revert to atomic number.
@@ -173,11 +267,21 @@ class Chemical:
 
     @staticmethod
     def get_acm(mol):
-        """Returns atom connectivity matrix of a molecule.
+        """Calculates atom connectivity matrix of a molecule.
 
         Atom connectivity matrix is a weighted adjacency matrix which
         diagonal elements indicates atom's atomic number and non-zero
         off-diagonal elements denotes bond order.
+
+        Parameters
+        ----------
+        mol : Molecule
+            A molecule for which
+
+        Returns
+        -------
+        matrix : numpy array
+            An array representing atom connectivity matrix.
         """
         matrix = numpy.zeros((len(mol.GetAtoms()), len(mol.GetAtoms())))
         for i, a in enumerate(mol.GetAtoms()):
