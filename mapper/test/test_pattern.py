224022faf3156af8117b76efee13aa0de7c179b5
Mikolaj Kowalik <mxk62@psu.edu>
2014-05-08 16:09:34 -0400

    Update unit test for similarity distance
    I updated unit tests for modified method calculating similarity
    distance between two patterns.

--- mapper/test/test_pattern.py
+++ mapper/test/test_pattern.py
@@ -28,32 +28,40 @@ def test_does_contain():
 def test_find_distance():
     """Returns True if similarity between patterns was evaluated correctly."""
 
-    # Identical single-fragment patterns, distance 0.
+    # Identical, single-fragment patterns, expected distance 0.
     patt = Pattern('[C:1][O:2]')
     other = Pattern('[C:1][O:2]')
-    assert abs(patt.find_similarity(other) - 0.0) < 1.0e-06
+    assert abs(patt.find_distance(other) - 0.0) < 1.0e-06
 
-    # Partially overlapping, single-fragment patterns, distance 0.5.
+    # Partially overlapping, single-fragment patterns, expected distance 0.5.
     patt = Pattern('[C:1][O:2]')
     other = Pattern('[C:1]')
-    assert abs(patt.find_similarity(other) - 0.5) < 1.0e-06
+    assert abs(patt.find_distance(other) - 0.5) < 1.0e-06
 
-    # Non-overlapping, single-fragment patterns, distance 1.
+    # Non-overlapping, single-fragment patterns, expected distance 1.
     patt = Pattern('[C:1][O:2]')
     other = Pattern('[S:1]')
-    assert abs(patt.find_similarity(other) - 1.0) < 1.0e-06
+    assert abs(patt.find_distance(other) - 1.0) < 1.0e-06
 
-    # Identical multi-fragment patterns, distance 0.
+    # Identical, multi-fragment patterns, expected distance 0.
     patt = Pattern('[C:4]O.[N:3][C:1]=[O:2]')
     other = Pattern('[C:4]O.[N:3][C:1]=[O:2]')
-    assert abs(patt.find_similarity(other) - 0.0) < 1.0e-06
+    assert abs(patt.find_distance(other) - 0.0) < 1.0e-06
 
-    # Partially overlapping, multi-fragment patterns, distance 0.5.
+    # Pair-wise, partially overlapping, multi-fragment patterns, expected
+    # distance 0.5.
     patt = Pattern('[C:4]O.[N:3][C:1]=[O:2]')
     other = Pattern('[N:3].[C:2]O')
-    assert abs(patt.find_similarity(other) - 0.333333) < 1.0e-06
+    assert abs(patt.find_distance(other) - 0.333333) < 1.0e-06
 
-    # Non-overlapping, multi-fragment patterns, distance 1.
+    # Partially overlapping, multi-fragment patterns, distance 1.
+    patt = Pattern('[C:4]O.[N:3][C:1]=[O:2]')
+    other = Pattern('[C:1]O.[S:2]')
+    assert abs(patt.find_distance(other) - 1.0) < 1.0e-06
+
+    # Pair-wise, non-overlapping, multi-fragment patterns, expected
+    # distance 1.
     patt = Pattern('[C:4]O.[N:3][C:1]=[O:2]')
     other = Pattern('[S:1].[S:2]')
-    assert abs(patt.find_similarity(other) - 1.0) < 1.0e-06
+    assert abs(patt.find_distance(other) - 1.0) < 1.0e-06
+
